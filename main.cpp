b5bb05edbc910743eb6cce8ab927cbc7e03ddb64
Alan Kagiri <akagiri20@shs.shschools.org>
2017-10-19 10:41:35 -0700

    I made chages

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
-#include <iostream>
+//THis is a game by Alankagiri
+#include <iostream> 
 using namespace std;
-int main() {
+int main() 
+{
     int width = 20, height = 15, x = 4, y = 5;
     char playerIcon = '@', userInput = 0;
-    while(userInput != 'q') {
-        for(int row = 0; row < height; row++) {
-            for(int col = 0; col < width; col++) {
+    while(userInput != 'q')
+	 {
+        for(int row = 0; row < height; row++) 
+		{
+            for(int col = 0; col < width; col++) 
+			{
                 if(row == y && col == x) cout << playerIcon;
                 else cout << '.';
             }
